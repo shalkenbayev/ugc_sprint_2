c8c855791be2dc936d02281b955d491e0241f89a
anderskate <andrey.starozhilov@mail.ru>
2021-12-09 11:35:11 +0500

    add elk logging with sentry

--- kafka_api/src/core/config.py
+++ kafka_api/src/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     app_name: str = Field("API для обработки данных", env="APP_NAME")
     app_version: str = Field("0.1.0", env="APP_VERSION")
 
-    kafka_bootstrap_servers: str = Field("kafka-1:9092", env="KAFKA_BOOTSTRAP_SERVERS")
+    kafka_bootstrap_servers: str = Field("broker:9092", env="KAFKA_BOOTSTRAP_SERVERS")
     movie_events_topic: str = "movie_events"
 
     log_level: str = Field("INFO", env="LOG_LEVEL")
